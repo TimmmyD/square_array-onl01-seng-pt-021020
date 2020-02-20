31ea9890a1affa50b2b2c667c6421769009896b7
Tim Donoghue <tmd112593@gmail.com>
2020-02-20 02:34:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  arr = []
+  array.map do |number|
+   arr << number ** 2 
+  end
+arr
 end
+
+
+
+
+  #numbers = [5,6,7,8]
+  #square_array(numbers)
+  
+  
